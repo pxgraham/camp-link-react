e9e5dee8c2374bd9c9477f270937156fda394412
RolandoD <r.delgado1203@gmail.com>
2019-01-02 21:04:54 -0700

    added user authentication

---
+++ client-camplink/src/app.config.js
@@ -0,0 +1,6 @@
+export default {
+    url: 'https://dev-146444.oktapreview.com',
+    issuer: 'https://dev-146444.oktapreview.com/oauth2/default',
+    redirect_uri: window.location.origin + '/implicit/callback',
+    client_id: '0oaiqcz2n3RNboU9d0h7'
+  };
