0b3795bd31d0ebc3141517f78287226c52a1f4f9
RolandoD <r.delgado1203@gmail.com>
2019-01-07 20:10:18 -0700

    removed okta auth, added node/mongodb auth with jwt and middleware

--- .old/api/app.js
+++ .old/api/app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const PORT = process.env.PORT || 3001;
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './styles/index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <Router>
+
+    </Router>,
+    document.getElementById('root')
+);
+registerServiceWorker();
