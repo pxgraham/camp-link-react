0fc2fc536551929cae9d75c7019259a09e7b57a2
Peyton <41251008+pxgraham@users.noreply.github.com>
2019-01-12 13:25:56 -0700

    added register form

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,15 +3,15 @@ import '../styles/login.css';
 
 function LoginForm() {
     return (
-        <form action="/action_page.php" className="login-form">
+        <form action="/action_page.php" method="POST" className="login-form">
             <h1>Log In:</h1>
             <label htmlFor="email"><b>Email/User</b></label>
-            <input type="text" placeholder="Enter Email/User" name="email" required />
+            <input type="text" placeholder="Enter Email/User" name="username" required />
             <label htmlFor="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required /><br /><br />
+            <input type="password" placeholder="Enter Password" name="password" required /><br /><br />
             <button type="submit" className="btn">Login</button>
             <p className="new-user">
-                <a href="../signup/signup.html">New user? Click here to sign up.</a>
+                <a href="/signup">New user? Click here to sign up.</a>
             </p>
         </form>
     );
