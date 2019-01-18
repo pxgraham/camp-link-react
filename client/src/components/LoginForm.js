a4c8ad0bec7cacdc50aba7ecfa4ea9bcb6112b14
Peyton <41251008+pxgraham@users.noreply.github.com>
2019-01-17 20:25:37 -0700

    added axios post to signup page and login page

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,20 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import '../styles/login.css';
 
-function LoginForm() {
-    return (
-        <form action="/users/authenticate" method='POST' target='/users/' className="login-form">
-            <h1>Log In:</h1>
-            <label htmlFor="email"><b>Email/User</b></label>
-            <input type="text" placeholder="Enter Email/User" name="username" required />
-            <label htmlFor="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="password" required /><br /><br />
-            <button type="submit" className="btn">Login</button>
-            <p className="new-user">
-                <a href="/signup">New user? Click here to sign up.</a>
-            </p>
-        </form>
-    );
+class LoginForm extends Component {
+    state = {
+        username: "",
+        hash: "",
+    }
+    handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+        console.log(this.state);
+      };
+    formPost = event => {
+        event.preventDefault();
+        axios({
+            method: 'post',
+            url: 'http://localhost:3001/users/authenticate',
+            data: JSON.stringify({
+                "username": "username", 
+                "hash": "password", 
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            json: true
+            //config: { headers: {'Content-Type': 'multipart/form-data' }}
+            })
+            .then(function (response) {
+                //handle success
+                alert(`Congrats you are now logged in!`)
+                console.log(response);
+            })
+            .catch(function (response) {
+                //handle error
+                console.log(response);
+            });
+    }
+    render() {
+        return (
+            <form className="login-form">
+                <h1>Log In:</h1>
+                <label htmlFor="email"><b>Email/User</b></label>
+                <input type="text" placeholder="Enter Email/User" onChange={this.handleInputChange} name="username" required />
+                <label htmlFor="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" onChange={this.handleInputChange} name="hash" required /><br /><br />
+                <button type="submit" className="btn" onClick={this.formPost}>Login</button>
+                <p className="new-user">
+                    <a href="/signup">New user? Click here to sign up.</a>
+                </p>
+            </form>
+        );
+    }
 }
 
 export default LoginForm;
