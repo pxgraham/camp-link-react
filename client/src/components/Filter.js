d5d733d63a1f0c301e94c3f4bfa8157c4cf1fefe
RolandoD <r.delgado1203@gmail.com>
2019-01-26 10:10:17 -0700

    added signout token removal

--- client/src/components/Filter.js
+++ client/src/components/Filter.js
@@ -1,35 +1,37 @@
-import React from "react";
-import {Autocomplete, Row, Input} from 'react-materialize';
+import React, { Component } from "react";
+import { Autocomplete, Row, Input } from 'react-materialize';
 
 const filterStyle = {
     position: 'absolute',
     right: '0px',
     marginTop: '10px',
 }
-function Filter() {
-  return (
-    <div>             
-        <Row>             
-            <Autocomplete
-                title='Search People'
-                data={
-                    {
-                        'Peyton Graham': 'images/people.png',
-                        'Microsoft': null,
-                        'Google': 'http://placehold.it/250x250'
-                    }
-                }
-            />  
-            <div style={filterStyle}>   
-                <div>&nbsp;&nbsp;Filter By</div>
-                <Input name='group1' type='checkbox' value='red' label='First Name' />
-                <Input name='group1' type='checkbox' value='yellow' label='Last Name' />
-                <Input name='group1' type='checkbox' value='green' label='Year'  />
-                <Input name='group1' type='checkbox' value='brown' label='Top Language' />
+class Filter extends Component {
+    render() {
+        return (
+            <div>
+                <Row>
+                    <Autocomplete
+                        title='Search People'
+                        data={
+                            {
+                                'Peyton Graham': 'images/people.png',
+                                'Microsoft': null,
+                                'Google': 'http://placehold.it/250x250'
+                            }
+                        }
+                    />
+                    <div style={filterStyle}>
+                        <div>&nbsp;&nbsp;Filter By</div>
+                        <Input name='group1' type='checkbox' value='red' label='First Name' />
+                        <Input name='group1' type='checkbox' value='yellow' label='Last Name' />
+                        <Input name='group1' type='checkbox' value='green' label='Year' />
+                        <Input name='group1' type='checkbox' value='brown' label='Top Language' />
+                    </div>
+                </Row>
+                <hr />
             </div>
-        </Row>   
-        <hr />          
-    </div>
-  );
+        );
+    }
 }
 export default Filter;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,25 @@
 import React from "react";
 import '../styles/navbar.css';
-const navStyle ={
-  fontSize: '15px',
+const navStyle = {
+    fontSize: '15px',
 }
+
+function signout() {
+    localStorage.key('token') ? localStorage.removeItem('token') : console.log(localStorage.key('token'))
+};
+
 function Navbar() {
     return (
         <div className="topnav responsive" id="myTopnav">
-        <a href="/" class="active">Home</a>
-        <a href="/login">Login</a>
-        <a href="/signup">Sign Up</a>
-        <a href="/directory">Directory</a>
-        <a href="#profile">My Profile</a>
-        <a href="#update">Update My Profile</a>
-        <a href="#signout">Sign Out</a>
-        <a href="javascript:void(0);" style={navStyle} class="icon" onclick="myFunction()">&#9776;</a>
-    </div>
+            <a href="/" className="active">Home</a>
+            <a href="/login">Login</a>
+            <a href="/signup">Sign Up</a>
+            <a href="/directory">Directory</a>
+            <a href="#profile">My Profile</a>
+            <a href="#update">Update My Profile</a>
+            <a href="/" onClick={signout}>Sign Out</a>
+            {/*         <a href="javascript:void(0);" style={navStyle} class="icon" onclick="myFunction()">&#9776;</a>
+ */}    </div>
     );
 }
 
