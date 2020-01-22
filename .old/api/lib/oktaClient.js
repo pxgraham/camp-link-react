276578648ec0c6dace0aba41d9a0629a0225dbdf
pxgraham <pxgraham@gmail.com>
2020-01-22 13:09:41 -0700

    initial commit for relaunch

--- .old/api/lib/oktaClient.js
+++
@@ -1,8 +0,0 @@
-const okta = require('@okta/okta-sdk-nodejs');
-
-const client = new okta.Client({
-  orgUrl: 'https://dev-146444.oktapreview.com',
-  token: '009NZNujr3WeIXxGlbSl96nj7eeh1r2J942xKfa4SH'
-});
-
-module.exports = client;
--- .old/app.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-    url: 'https://dev-146444.oktapreview.com',
-    issuer: 'https://dev-146444.oktapreview.com/oauth2/default',
-    redirect_uri: window.location.origin + '/implicit/callback',
-    client_id: '0oaiqcz2n3RNboU9d0h7'
-  };
